cfb1bf69d9fb0710825b55c3c4d231ec538e53e7
zx <zxmainwy@163.com>
2015-11-03 13:43:14 +0800

    first commit

---
+++ ExpandViewsLib/src/com/expand/library/internal/Model.java
@@ -0,0 +1,35 @@
+package com.expand.library.internal;
+
+import java.util.List;
+
+public class Model {
+
+	private List<Model> modelChilds;
+	private String modelName;
+	private String modelImgUrl;
+
+	public List<Model> getModelChilds() {
+		return modelChilds;
+	}
+
+	public void setModelChilds(List<Model> modelChilds) {
+		this.modelChilds = modelChilds;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public String getModelImgUrl() {
+		return modelImgUrl;
+	}
+
+	public void setModelImgUrl(String modelImgUrl) {
+		this.modelImgUrl = modelImgUrl;
+	}
+
+}
---
+++ ExpandViewsLib/src/com/expand/library/internal/ModelPosition.java
@@ -0,0 +1,24 @@
+package com.expand.library.internal;
+
+public class ModelPosition {
+
+	private int parentPosition;
+	private int childPosition;
+
+	public int getParentPosition() {
+		return parentPosition;
+	}
+
+	public void setParentPosition(int parentPosition) {
+		this.parentPosition = parentPosition;
+	}
+
+	public int getChildPosition() {
+		return childPosition;
+	}
+
+	public void setChildPosition(int childPosition) {
+		this.childPosition = childPosition;
+	}
+
+}
